2d565cc8d150cb66cf7ddf9c9d1521b4be157fcb
yohann.schatt <yohann.schatt@etu.umontpellier.fr>
2024-12-10 17:15:08 +0100

    Dao fini

---
+++ src/main/java/waveon/waveon/connector/PGconnector.java
@@ -0,0 +1,25 @@
+package waveon.waveon.connector;
+
+import java.sql.*;
+
+public class PGconnector {
+
+    private static final String URL = "jdbc:postgresql://cluster-ig4.igpolytech.fr:30017/waveon_db";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "86ff479c8e85cc70f0c7aa5868e0c04d";
+
+    private static PGconnector instance = null;
+
+    public PGconnector() {}
+
+    public static PGconnector getInstance(){
+        if (instance == null) {
+            instance = new PGconnector();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
--- src/main/java/waveon/waveon/persist/ArtistDAOPG.java
+++ src/main/java/waveon/waveon/persist/ArtistDAOPG.java
@@ -1,8 +1,12 @@
 package waveon.waveon.persist;
 
+import waveon.waveon.connector.PGconnector;
 import waveon.waveon.core.Artist;
+import waveon.waveon.core.OrdUser;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  * 
@@ -25,7 +29,19 @@ public class ArtistDAOPG implements ArtistDAO {
      * @return
      */
     public Artist getArtistByEmail(String email) {
-        // TODO implement here
+        PGconnector pg = PGconnector.getInstance();
+        String sql = "SELECT * FROM artist WHERE email = ?";
+        try (Connection conn = pg.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, email);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Artist artist = new Artist(rs.getInt("id"), rs.getString("username"), rs.getString("email"), rs.getString("password"));
+                return artist;
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Error in OrdUserDAOPG.getUserByEmail : " + e);
+        }
         return null;
     }
 }
