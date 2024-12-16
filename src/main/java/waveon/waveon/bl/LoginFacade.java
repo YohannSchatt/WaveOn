45e4956c850238a9119a58ca891ee01e33b95df4
CorentinDelclaud <corentin.delclaud@gmail.com>
2024-12-16 15:31:20 +0100

    ajout de la mainPage

--- src/main/java/waveon/waveon/bl/LoginFacade.java
+++ src/main/java/waveon/waveon/bl/LoginFacade.java
@@ -2,19 +2,19 @@ package waveon.waveon.bl;
 
 import waveon.waveon.core.IUser;
 import waveon.waveon.core.OrdUser;
-import waveon.waveon.core.User;
 import waveon.waveon.persist.AbstractFactory;
 import waveon.waveon.persist.ArtistDAO;
 import waveon.waveon.persist.OrdUserDAO;
-import waveon.waveon.persist.PGFactory;
 
 public class LoginFacade {
     private AbstractFactory factory;
     private OrdUserDAO userDAO;
     private ArtistDAO artistDAO;
 
-    private IUser currentUser;
 
+    private static OrdUser faux = new OrdUser(0, "coco", "coco@gmail.com","21032004");
+
+    private static IUser currentUser = faux;
     public LoginFacade() {
         factory = AbstractFactory.getInstance();
         assert factory != null;
@@ -27,16 +27,16 @@ public class LoginFacade {
         // Utilisez la valeur de isArtist selon vos besoins
         if (isArtist) {
             System.out.println("L'utilisateur est un artiste.");
-            this.currentUser = artistDAO.getArtistByEmail(email);
+            currentUser = artistDAO.getArtistByEmail(email);
         } else {
             System.out.println("L'utilisateur n'est pas un artiste.");
-            this.currentUser = userDAO.getUserByEmail(email);
+            currentUser = userDAO.getUserByEmail(email);
         }
     }
 
     public boolean checkCredentials(String email, String password) {
         if (currentUser != null) {
-            if (this.currentUser.getPassword().equals(password) && this.currentUser.getEmail().equals(email)) {
+            if (currentUser.getPassword().equals(password) && currentUser.getEmail().equals(email)) {
                 return true;
             } else {
                 System.out.println("Login échoué : identifiants incorrects.");
@@ -44,5 +44,13 @@ public class LoginFacade {
             }
         }
         return false;
+    }
+
+    public IUser getCurrentUser() {
+        return currentUser;
+    }
+
+    public void logout() {
+        currentUser = null;
     }
 }
