a1c57783e1d50886da0d9fd6f97830d44d43c96b
Lennon Herrmann <lennonherrmann@hotmail.com>
2024-12-20 11:15:13 +0100

    ajout de coverImage et d'autres données lors de l'ajout d'une musique

--- src/main/java/waveon/waveon/core/Music.java
+++ src/main/java/waveon/waveon/core/Music.java
@@ -1,14 +1,24 @@
 package waveon.waveon.core;
 
+import java.sql.Date;
+
 public class Music {
     private final String title;
     private final byte[] fileContent; // Contenu binaire du fichier
+    private final byte[] coverMusic; // Contenu binaire de la pochette
     private final int artistId;
+    private final String artistName;
+    private final Date releaseDate;
+    private int stream_count;
 
-    public Music(String title, byte[] fileContent, int artistId) {
+    public Music(String title, byte[] fileContent, byte[] coverMusic, int artistId, String artistName, Date releaseDate) {
         this.title = title;
         this.fileContent = fileContent;
+        this.coverMusic = coverMusic;
         this.artistId = artistId;
+        this.artistName = artistName;
+        this.releaseDate = releaseDate;
+        this.stream_count = 0;
     }
 
     public String getTitle() {
@@ -19,7 +29,24 @@ public class Music {
         return fileContent;
     }
 
+    public byte[] getCoverMusic() {
+        return coverMusic;
+    }
+
     public int getArtistId() {
         return artistId;
     }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public int getStreamCount() {
+        return stream_count;
+    }
 }
+
--- src/main/java/waveon/waveon/persist/MusicDAOPG.java
+++ src/main/java/waveon/waveon/persist/MusicDAOPG.java
@@ -11,12 +11,16 @@ public class MusicDAOPG implements MusicDAO {
     @Override
     public boolean saveMusic(Music music) {
         PGconnector pg = PGconnector.getInstance();
-        String sql = "INSERT INTO music (title, file_content, artist_id) VALUES (?, ?, ?)";
+        String sql = "INSERT INTO music (title, file_content, cover_image, artist_id, artist_name, release_date, stream_count) VALUES (?, ?, ?, ?, ?, ?, ?)";
 
         try (Connection conn = pg.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, music.getTitle());
             pstmt.setBytes(2, music.getFileContent()); // Insertion du contenu binaire
-            pstmt.setInt(3, music.getArtistId());
+            pstmt.setBytes(3, music.getCoverMusic()); // Insertion du contenu binaire
+            pstmt.setInt(4, music.getArtistId());
+            pstmt.setString(5, music.getArtistName());
+            pstmt.setDate(6, music.getReleaseDate());
+            pstmt.setInt(7, music.getStreamCount());
             pstmt.executeUpdate();
             return true;
         } catch (SQLException e) {
